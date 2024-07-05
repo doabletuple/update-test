3277ffe91ee8519a97fc7aefc703894f4fcc7d3c
doabletuple <doabletuple@gmail.com>
2024-07-05 23:45:12 +0900

    v1.0.0

---
+++ UpdateTest.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         UpdateTest
+// @version      1.0.0
+// @description  Userscript update test
+// @author       doabletuple
+// @homepageURL  https://github.com/doabletuple/update-test
+// @match        http://*/*
+// @grant        none
+// @updateURL    https://github.com/doabletuple/update-test/releases/latest/download/UpdateTest.meta.js
+// @downloadURL  https://github.com/doabletuple/update-test/releases/latest/download/UpdateTest.user.js
+// ==/UserScript==
+
+console.log("------------------------------------------------------------");
+console.log("1.0.0");
+console.log("------------------------------------------------------------");
